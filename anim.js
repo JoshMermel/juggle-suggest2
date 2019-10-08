6fa1507e9bd7c767b02541a3cb4cf39079d3d2c7
Josh Mermelstein <joshmermel@gmail.com>
2019-10-08 21:26:22 +0100

    another idea on colors

--- anim.js
+++ anim.js
@@ -331,10 +331,24 @@ function setSiteswap(siteswap, color_by_orbit, is_sync) {
   }
 }
 
+var palette=[ "#000000", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6",
+  "#A30059", "#7A4900", "#0000A6", "#B79762", "#004D43", "#997D87", "#5A0007",
+  "#809693", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80", "#61615A",
+  "#BA0900", "#6B7900", "#00C2A0", "#B903AA", "#D16100", "#000035", "#7B4F4B",
+  "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F", "#372101", "#FFB500",
+  "#A079BF", "#CC0744", "#C0B9B2", "#001E09", "#00489C", "#6F0062", "#0CBD66",
+  "#456D75", "#B77B68", "#7A87A1", "#788D66", "#885578", "#FF8A9A", "#D157A0",
+  "#BEC459", "#456648", "#0086ED", "#886F4C", "#34362D", "#B4A8BD", "#00A6AA",
+  "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81", "#575329", "#B05B6F",
+  "#3B9700", "#04F757", "#C8A1A1", "#1E6E00", "#7900D7", "#A77500", "#6367A9",
+  "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700", "#549E79", "#201625",
+  "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329", "#5B4534", "#404E55",
+  "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C"]
+
 function recolorRandomly() {
-  var palette = new DistinctColors({count: balls.length, lightMin: 50});
+  let local_palette = shuffle(palette);
   for (let i = 0; i < balls.length; i++) {
-    balls[i].color = palette[i];
+      balls[i].color = local_palette[i];
   }
 }
 
@@ -347,7 +361,7 @@ function numOrbits() {
 }
 
 function recolorByOrbit() {
-  var palette = new DistinctColors({count: numOrbits(), lightMin: 50 });
+  let local_palette = shuffle(palette);
   for (let ball of balls) {
     ball.color = palette[ball.orbit.id];
   }
